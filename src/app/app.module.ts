0898e54f386d0ab0acd4a9458cb4426bc4ca8ace
xamfy <spratyaksh22@gmail.com>
2018-10-30 00:35:03 +0530

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
